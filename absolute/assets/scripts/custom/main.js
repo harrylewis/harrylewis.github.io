3e4e4fe397246a51a2abb276a91ee44c8b4bb321
Harry Lewis <harry.lewis@queensu.ca>
2017-04-02 22:30:15 -0400

    Getting to version 1 of Absolute Hockey School site

---
+++ absolute/assets/scripts/custom/main.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  $('.testimonials').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidestoShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    arrows: false
+  });
+
+});
